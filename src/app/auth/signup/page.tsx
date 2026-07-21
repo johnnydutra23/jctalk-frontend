9b6f339be61df0a9d4e66ddaf2ff54302c386746
johnnydutra23 <johnny.ardutra@gmail.com>
2026-07-21 18:21:47 -0300

    SignUp & SignIn finalizado

---
+++ src/app/auth/signup/page.tsx
@@ -0,0 +1,10 @@
+import { SignUpPage } from "@/components/Pages/SignUp";
+import { Metadata } from "next";
+
+export const metada: Metadata = {
+  title: "Registrar-se",
+};
+
+const SignUp = () => <SignUpPage />;
+
+export default SignUp;
