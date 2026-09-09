67121645e1cbf6bd8ce32859efb5a756724a32eb
johnnydutra23 <johnny.ardutra@gmail.com>
2026-09-09 09:48:34 -0300

    Corrigir nome da exportação metadata em account/page.tsx

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -1,7 +1,7 @@
 import { AccountPage } from "@/components/Pages/Account";
 import { Metadata } from "next";
 
-export const metada: Metadata = {
+export const metadata: Metadata = {
   title: "Minha conta",
 };
 
